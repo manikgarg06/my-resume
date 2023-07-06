e8279a5f066da09ab2aa390c5bd5bee071a364d0
manikgarg <manikgarg06>
2023-07-07 03:01:25 +0530

    Resume

---
+++ js/commands.js
@@ -0,0 +1,110 @@
+// These are the variable for the direct links
+var twitter = "https://www.twitter.com/manikgarg06";
+var linkedin = "https://www.linkedin.com/in/manikgarg06";
+var instagram = "https://www.instagram.com/manik._garg";
+var email = "mailto:manikgarg06@gmail.com";
+var shop = "https://node-complete-shop-guide.herokuapp.com/";
+var burger = "https://react-burger-app-5d664.web.app/";
+var resume = "/files/my-resume.pdf";
+var letsflip = "https://letsflip.web.app/";
+
+var p1 = "ude.my/UC-2650ed72-5bf7-4661-8d83-299000b01b8b"
+var p2 = "https://udemy-certificate.s3.amazonaws.com/pdf/UC-329820f0-8cb5-4db0-bc88-941386090930.pdf"
+var p3="https://udemy-certificate.s3.amazonaws.com/pdf/UC-7b38a4ae-b6b6-418f-9096-e7c5550cb966.pdf"
+var p4="https://udemy-certificate.s3.amazonaws.com/pdf/UC-ZO8MGMMO.pdf"
+var p5 ="https://udemy-certificate.s3.amazonaws.com/pdf/UC-5bbf4a49-7f4a-421c-b3df-71ccb4e7e1b1.pdf"
+
+// This content is about the "WHOIS"
+whois = [
+  "<br>",
+  "- Manik Garg is a Full Stack Developer, working as a Technical Lead at Paytm.",
+  "<br>",
+  "- He has a Bachelor's degree in Computer Science and Engineering.",
+  "<br>",
+  "- Manik specializes in Frontend development with the latest technologies such as React, Next and Angular Js.",
+  "<br>",
+  "- He has created the docker containers, networks and the docker images to deploy the complete BE Microservices.",
+  "<br>",
+  "- He has developed the Nagad ECOM Payment Gateway with Next.js to handle in-house transactions.",
+  "<br>",
+  "- He has integrated the Master Card Gateway in the Nagad Wallet to handle Third Party Transactions.",
+  "<br>",
+  "- Currently he is developing the complete website integrated with CMS, SEO Optimised and performance efficient.",
+  "<br>",
+  "- Manik is an active member of the Linkedin family. Always tries to impart the knowledge with the community.",
+  "<br>",
+];
+
+courses = [
+  "<br>",
+  '1. <a href="' + p1 + '" target="_blank">Next.js & React Complete Guide 2022' + "</a>",
+  '2. <a href="' + p2 + '" target="_blank">Node Js (Incl. Express, MongoDB) 2020' + "</a>",
+  '3. <a href="' + p3 + '" target="_blank">React Js (incl. Redux, Hooks, React Router) 2020' + "</a>",
+  '4. <a href="' + p5 + '" target="_blank">React Native - The Practical Guide 2020' + "</a>",
+  '5. <a href="' + p4 + '" target="_blank">Full Stack with Spring Boot and Angular 2019' + "</a>",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/manikgarg06' +
+    "</a>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="_blank">twitter/manikgarg06' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="_blank">instagram/manik._garg' +
+    "</a>",
+  "<br>",
+];
+
+
+projects = [
+    "<br>",
+    '1. Shop App <a href="' + shop + '" target="_blank">node/shop-app' + "</a>",
+    '2. Burger App <a href="' + burger + '" target="_blank">react/burger-app' + "</a>",
+    '3. Lets Flip the Card <a href="' + letsflip + '" target="_blank">react/letsflipthecard' + "</a>",
+    "<br>"
+  ];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>",
+];
+
+experiences = [
+    "<br>",
+    "- Currently working as a Technical Lead at Paytm since June 2021 (June 2021 - xx).",
+    "<br>",
+    "- Short stint at Newgen as a Senior Software Engineer (Dec 2020 - June 2021).",
+    "<br>",
+    "- Started my career as a Software Engineer at Infosys and worked for 3 years (Jan 2018 - Dec 2020).",
+    "<br>",
+]
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>            About Me',
+  '<span class="command">social</span>           My Social platforms',
+  '<span class="command">projects</span>         My Open Source Projects',
+  '<span class="command">resume</span>           My Resume',
+  '<span class="command">courses</span>          How I prepared ?',
+  '<span class="command">experiences</span>      What I did ?',
+  '<span class="command">email</span>            My Email address',
+  '<span class="command">clear</span>            Clear terminal',
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  "+-+-+-+-+-+-+-+-+-+-+-",
+  "|M|A|N|I|K| |G|A|R|G|",
+  "+-+-+-+-+ +-+-+-+-+-+-+",
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  '<span class="color2">Note: Don\'t think about</span> <span class="command">\'SUDO\'</span><span class="color2">.</span>',
+];
